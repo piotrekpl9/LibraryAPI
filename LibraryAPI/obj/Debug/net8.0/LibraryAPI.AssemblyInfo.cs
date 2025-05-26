1025806ab39c0412217a21b788d96c1713dfbece
piotrekpl9 <ppluta284@gmail.com>
2025-05-26 22:36:54 +0200

    ..

--- LibraryAPI/obj/Debug/net8.0/LibraryAPI.AssemblyInfo.cs
+++ LibraryAPI/obj/Debug/net8.0/LibraryAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6389902868baa55c12aae93d7f8d055142f6a65")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
